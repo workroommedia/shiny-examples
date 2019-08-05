cd73d96fb14110fca3267c4e154bdf266fee0bbe
workroommedia <4570500+workroommedia@users.noreply.github.com>
2019-08-05 09:46:04 -0400

    Update server.R

--- 063-superzip-example/server.R
+++ 063-superzip-example/server.R
@@ -22,7 +22,7 @@ function(input, output, session) {
         urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
         attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
       ) %>%
-      setView(lng = -93.85, lat = 37.45, zoom = 4)
+      setView(lng = 34.8021, lat = 38.9968, zoom = 4)
   })
 
   # A reactive expression that returns the set of zips that are
